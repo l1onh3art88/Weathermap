0124c39ccfda2410cc956bda76a30a7541120e55
l1on3hart88 <spenccheng@gmail.com>
2015-08-14 13:57:52 +0000

    loaded angular and bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular/angular
 //= require_tree .
+
+angular.module('WeatherMap',[]).controller('myController', function(){
+  
+});
